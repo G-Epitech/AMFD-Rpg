e3154d7e9e3ad84512652920db034436643cbc56
sdrago1 <dragos.suceveanu@epitech.eu>
2023-04-03 20:15:44 +0200

    ✨ Adding movement collision system

--- src/app/core/movement.c
+++ src/app/core/movement.c
@@ -9,11 +9,12 @@
 #include "types/players/players.h"
 #include "app/events/types.h"
 
-static int movement_is_possible(sfVector2f position, int **collision_layer)
+static int movement_is_possible(sfVector2f position, sfVector2f offset,
+int **collision_layer)
 {
-    if (collision_layer[(int)position.y][(int)position.x])
-        return 1;
-    return 0;
+    position.x += offset.x / 16;
+    position.y += offset.y / 16;
+    return (collision_layer[(int)position.y][(int)position.x] == 1);
 }
 
 static sfVector2f increment_position(sfVector2f position, sfVector2f offset)
@@ -27,9 +28,12 @@ void core_handle_movement(player_t *player, control_t *control,
 int **collision_layer)
 {
     for (size_t i = 0; i < 4; i++) {
-        if (control[i].direction) {
+        if (control[i].direction && movement_is_possible(player->colision_pos,
+        control[i].offset, collision_layer)) {
             player->position = increment_position(player->position,
             control[i].offset);
+            player->colision_pos = increment_position(player->colision_pos,
+            (sfVector2f) {control[i].offset.x / 16, control[i].offset.y / 16});
         }
     }
 }
