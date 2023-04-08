8d71c4ae5345292370122abefc992d9533f735b9
TekMath <dokuhebi.game@gmail.com>
2023-04-08 18:56:53 +0200

    🐛 Fixed destroy window

--- src/app/settings/fullscreen.c
+++ src/app/settings/fullscreen.c
@@ -5,6 +5,7 @@
 ** Music settings
 */
 
+#include <stdio.h>
 #include "types/renderer/types.h"
 #include "app/types.h"
 
@@ -14,7 +15,9 @@ int settings_fullscreen(renderer_t *renderer, app_t *app, lever_t *lever)
     app->settings->full_screen = !app->settings->full_screen;
     lever->active = app->settings->full_screen;
     sfRenderWindow_close(renderer->window);
+    sfRenderWindow_destroy(renderer->window);
     renderer->window = window_init(app->settings->full_screen);
     renderer->objects->window = renderer->window;
+    renderer->default_view = sfRenderWindow_getDefaultView(renderer->window);
     return 0;
 }
