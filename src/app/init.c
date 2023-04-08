2bf435839faf8ee0198d522ecdfb548ed64dc23e
TekMath <dokuhebi.game@gmail.com>
2023-04-08 19:17:14 +0200

    🐛 Fixed states

--- src/app/init.c
+++ src/app/init.c
@@ -59,7 +59,7 @@ app_t *app_init(void)
 
     if (!app)
         return NULL;
-    app->state = ST_INVENTORY;
+    app->state = ST_MAIN_MENU;
     app->world = WL_VILLAGE;
     app->control = init_controller();
     app->players = players_list_init();
