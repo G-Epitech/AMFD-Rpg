abfcf5efc2cd25e58edb26ab971d49bebad829eb
Flavien Chenu <flavien.chenu@epitech.eu>
2023-04-10 14:56:53 +0200

    📦  Fixed CJSON lib object creation and set props

--- lib/cjson/include/cjson.h
+++ lib/cjson/include/cjson.h
@@ -17,5 +17,10 @@
     #include "types/object.h"
 
     #define CJSON(v) ((cjson_value_t) v)
+    #define CJSON_NUMBER(v) CJSON((float) v)
+    #define CJSON_STRING(v) CJSON((char *) v)
+    #define CJSON_OBJECT(v) CJSON((cjson_t *) v)
+    #define CJSON_ARRAY(v) CJSON((cjson_array_t *) v)
+    #define CJSON_BOOL(v) CJSON((bool) v)
 
 #endif /* !CJSON_H_ */
