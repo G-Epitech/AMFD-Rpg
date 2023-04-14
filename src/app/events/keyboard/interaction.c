96af947c4373b1faf35b53e4c67b35a9ef7f7b40
TekMath <dokuhebi.game@gmail.com>
2023-04-14 18:18:41 +0200

    ✨ Added fight event

--- src/app/events/keyboard/interaction.c
+++ src/app/events/keyboard/interaction.c
@@ -5,14 +5,29 @@
 ** Keyboard interaction
 */
 
+#include <stdio.h>
 #include <SFML/Graphics.h>
 #include "app/app.h"
 #include "types/players/types.h"
 #include "app/events/types.h"
+#include "types/npc/npc.h"
+
+static void fight_event(interactions_t *interactions, app_t *app)
+{
+    npc_data_t *data;
+
+    if (interactions->type != IT_NPC)
+        return;
+    data = interactions->data.npc;
+    // Display dialogs
+    if (data->enemy) {
+        app->state = ST_FIGHT;
+    }
+}
 
 void keyboard_interaction(sfEvent event, app_t *app)
 {
-    if (event.key.code == sfKeyE && app->interaction->interaction) {
-        //Code
+    if (event.key.code == sfKeyF && app->interaction->interaction) {
+        fight_event(app->interaction, app);
     }
 }
