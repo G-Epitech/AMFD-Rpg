5fdad6a93eefe62fd09ea7cbd54ea43b90740170
TekMath <dokuhebi.game@gmail.com>
2023-04-16 20:50:16 +0200

    💡 Added animations text display

---
+++ src/app/display/animations/animations.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** animations.c
+** File description:
+** Display animations
+*/
+
+#include "types/renderer/types.h"
+#include "app/types.h"
+#include "app/display/display.h"
+
+void display_animations(renderer_t *renderer, app_t *app)
+{
+    display_animations_floating_text(renderer, app);
+}
