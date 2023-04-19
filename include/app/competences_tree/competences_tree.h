ea53b4bff847b4f5756ce15a68253536c66766db
Fradet <axel.fradet@epitech.eu>
2023-04-19 09:55:47 +0200

    🏷️  Created header file for the competence tree

---
+++ include/app/competences_tree/competences_tree.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** core.h
+** File description:
+** Core functions
+*/
+
+#ifndef CORE_H_
+    #define CORE_H_
+
+    #include "app/types.h"
+    #include "app/app.h"
+    #include "types/renderer/types.h"
+
+
+#endif /* !CORE_H_ */
---
+++ include/app/competences_tree/types.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** RPG
+** File description:
+** types
+*/
+
+#ifndef TYPES_H_
+    #define TYPES_H_
+
+    #include <SFML/Graphics.h>
+
+
+#endif /* !TYPES_H_ */
