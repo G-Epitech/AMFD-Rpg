8da64033f1b768ed9e0b51174760ec0f89efc463
Fradet <axel.fradet@epitech.eu>
2023-04-21 00:57:51 +0200

    🐛 Fixed probleme about task go buster

--- src/app/display/tasks/camera/result.c
+++ src/app/display/tasks/camera/result.c
@@ -16,29 +16,21 @@
 void display_win(renderer_t *renderer, app_t *app)
 {
     renderer_objects_t *objects = renderer->objects;
-    task_t *node = find_task_node(app, 4);
 
+    (void) app;
     sfText_setPosition(objects->text, (sfVector2f) {785, 550});
     sfText_setColor(objects->text, sfWhite);
     sfText_setString(objects->text, "Vous avez gagne");
     sfRenderWindow_drawText(renderer->window, objects->text, NULL);
-    if (CAMERA_TIME_ENDED(node) + 3 < TIME_FLOAT(node->content.camera)) {
-        app->state = ST_INGAME;
-        reset_setup_camera(app);
-    }
 }
 
 void display_loose(renderer_t *renderer, app_t *app)
 {
     renderer_objects_t *objects = renderer->objects;
-    task_t *node = find_task_node(app, 4);
 
+    (void) app;
     sfText_setPosition(objects->text, (sfVector2f) {785, 550});
     sfText_setColor(objects->text, sfWhite);
     sfText_setString(objects->text, "Vous avez perdu");
     sfRenderWindow_drawText(renderer->window, objects->text, NULL);
-    if (CAMERA_TIME_ENDED(node) + 3 < TIME_FLOAT(node->content.camera)) {
-        app->state = ST_INGAME;
-        reset_setup_camera(app);
-    }
 }
