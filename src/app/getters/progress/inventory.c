06bdb21d82bdfa3519d5fbf400b4e35ba9d0f60a
Flavien Chenu <flavien.chenu@epitech.eu>
2023-04-20 18:54:16 +0200

    💡Updated levels display and added max levels on player

--- src/app/getters/progress/inventory.c
+++ src/app/getters/progress/inventory.c
@@ -9,26 +9,33 @@
 
 int inventory_get_xp(app_t *app, progress_t *progress)
 {
-    (void) app;
+    float ratio = (float) app->player->xp / (float) app->player->xp_max;
+
     (void) progress;
-    return 68;
+    return (ratio * 100);
 }
 
 int inventory_get_life(app_t *app, progress_t *progress)
 {
+    float ratio = (float) app->player->life / (float) app->player->life_max;
+
     (void) progress;
-    return app->player->life * 100 / 20;
+    return (ratio * 100);
 }
 
 int inventory_get_intelligence(app_t *app, progress_t *progress)
 {
-    (void) app;
+    float ratio = (float) app->player->intellect;
+
     (void) progress;
-    return app->player->intellect * 100 / 10;
+    ratio /= (float) app->player->intellect_max;
+    return (ratio * 100);
 }
 
 int inventory_get_speed(app_t *app, progress_t *progress)
 {
+    float ratio = (float) app->player->speed / (float) app->player->speed_max;
+
     (void) progress;
-    return app->player->speed * 100 / 10;
+    return (ratio * 100);
 }
--- src/types/players/add.c
+++ src/types/players/add.c
@@ -12,6 +12,19 @@
 #include "types/players/types.h"
 #include "types/characters/types.h"
 
+static void init_levels(player_t *player)
+{
+    player->xp = 10;
+    player->level = 15;
+    player->life = 20;
+    player->intellect = 7;
+    player->speed = 8;
+    player->intellect_max = 100;
+    player->speed_max = 100;
+    player->life_max = 100;
+    player->xp_max = 50;
+}
+
 player_t *players_add(list_t *list, char *name)
 {
     player_t *player = malloc(sizeof(player_t));
@@ -25,11 +38,7 @@ player_t *players_add(list_t *list, char *name)
     player->skin_id = -1;
     player->orientation = SO_LEFT;
     player->name = my_strdup(name);
-    player->xp = 1;
-    player->level = 15;
-    player->life = 20;
-    player->intellect = 7;
-    player->speed = 8;
+    init_levels(player);
     list_append(list, node_new((node_data_t) player));
     return player;
 }
