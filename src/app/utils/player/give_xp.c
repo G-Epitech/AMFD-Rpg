457f08b820dc7b9bb10d25bca4756be911e25acc
TekMath <dokuhebi.game@gmail.com>
2023-04-23 05:14:40 +0200

    🍌 Fixed utils give xp function coding-style

--- src/app/utils/player/give_xp.c
+++ src/app/utils/player/give_xp.c
@@ -12,6 +12,23 @@
 #include "app/animations/animations.h"
 #include "my/include/my.h"
 
+static void level_update(renderer_t *renderer, app_t *app)
+{
+    list_t *events = animation_event_new(app);
+    int max = app->player->xp_max;
+    char *description = NULL;
+
+    app->player->xp = app->player->xp - max;
+    app->player->level++;
+    app->player->credits++;
+    app->player->xp_max = app->player->level * 10;
+    description = my_strcat("Vous venez de passer au\nniveau ",
+    nbr_to_str(app->player->level));
+    animations_notif_add(events, renderer->ressources->icons->xp,
+    "Nouveau niveau", my_strdup(description));
+    free(description);
+}
+
 void utils_give_xp(renderer_t *renderer, app_t *app, int xp)
 {
     list_t *events = animation_event_new(app);
@@ -20,19 +37,12 @@ void utils_give_xp(renderer_t *renderer, app_t *app, int xp)
 
     app->player->xp += xp;
     if (app->player->xp >= max) {
-        app->player->xp = app->player->xp - max;
-        app->player->level++;
-        app->player->credits++;
-        app->player->xp_max = app->player->level * 10;
-        description = my_strcat("Vous venez de passer au\nniveau ",
-        nbr_to_str(app->player->level));
-        animations_notif_add(events, renderer->ressources->icons->xp,
-        "Nouveau niveau", my_strdup(description));
+        level_update(renderer, app);
     } else {
         description = my_strcat("Vous venez de gagner ", nbr_to_str(xp));
         description = my_strcat(description, " XP.");
         animations_notif_add(events, renderer->ressources->icons->xp,
         "Gain experience", my_strdup(description));
+        free(description);
     }
-    free(description);
 }
