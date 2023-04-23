d131e7a32e052b9936abf487cdeb3a829a62638f
TekMath <dokuhebi.game@gmail.com>
2023-04-23 19:21:59 +0200

    ✨ Added dialog box to IP address connexion

--- src/app/network/connexion/join.c
+++ src/app/network/connexion/join.c
@@ -10,21 +10,38 @@
 #include "types/renderer/types.h"
 #include "app/types.h"
 #include "types/players/players.h"
+#include "types/dialog_box/dialog_box.h"
 
-int network_connexion_join(renderer_t *renderer, app_t *app, button_t *button)
+static void network_connexion_join_start(app_t *app, char *input)
 {
-    sfIpAddress server = sfIpAddress_LocalHost;
+    sfIpAddress server;
     sfTcpSocket* socket = sfTcpSocket_create();
 
-    (void) renderer;
-    (void) button;
+    if (!input)
+        return;
+    server = sfIpAddress_fromString(input);
     if (sfTcpSocket_connect(socket, server, 53000, sfTime_Zero) !=
     sfSocketDone) {
         app->state = ST_GAMEMODE;
-        return 1;
+        return;
     }
     app->network->socket = socket;
     app->state = ST_CHOICE;
     app->partner = players_add(app->players, "Player2");
+}
+
+int network_connexion_join(renderer_t *renderer, app_t *app, button_t *button)
+{
+    (void) renderer;
+    (void) button;
+    app->dialog_box->show = true;
+    dialog_box_reset_events(app->dialog_box, true);
+    dialog_box_set_message(app->dialog_box, "Saisissez l'IP du joueur");
+    app->dialog_box->mode = DGBOX_INPUT;
+    dialog_box_set_input(app->dialog_box, "127.0.0.1");
+    dialog_box_set_option1(app->dialog_box, "Connexion");
+    dialog_box_set_option2(app->dialog_box, "Annuler");
+    dialog_box_set_option3(app->dialog_box, NULL);
+    app->dialog_box->onoption1 = &network_connexion_join_start;
     return 0;
 }
