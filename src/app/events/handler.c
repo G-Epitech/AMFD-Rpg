26cb2adf11c70422ad2ad5dd9b35c77897b43c8e
Fradet <axel.fradet@epitech.eu>
2023-04-23 15:15:55 +0200

    🐛 Checked all task

--- src/app/events/handler.c
+++ src/app/events/handler.c
@@ -24,7 +24,7 @@ renderer_t *renderer)
     event_mouse_button_released(app, renderer, event);
     event_mouse_moved(app, renderer, event);
     if (event.type == sfEvtMouseWheelScrolled)
-        app->state = ST_TASK_BASH;
+        app->state = ST_TASK_BRUTEFORCE;
 }
 
 void event_handler(sfRenderWindow *window, app_t *app, renderer_t *renderer)
